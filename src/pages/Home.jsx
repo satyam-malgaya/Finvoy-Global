58aa87024f88398c3e6c3a2cef85a754ce1e4cdb
Hariom Choudhary <165458145+Hariom93@users.noreply.github.com>
2025-11-25 16:41:02 +0530

    Home.js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,7 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>
-      <div>
-        home
-      </div>
-      <div>
-        main 
-      </div>
-      <div>
-        hariom
-      </div>
-      <div>
-        about
-      </div>
 
-    </div>
-  )
 }
 
 export default Home
