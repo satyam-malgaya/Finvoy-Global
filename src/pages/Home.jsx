561293813e24fb3b15b50ee70ed0fa7312084eda
satyam-malgaya <satyam.malgaya@gmail.com>
2025-11-25 16:19:19 +0530

    add contain

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      
+      <div>
+        home
+      </div>
+      <div>
+        main 
+      </div>
+      <div>
+        hariom
+      </div>
+      <div>
+        about
+      </div>
+
     </div>
   )
 }
