24265ec9d58c8f5d117164df9a358fac031e33ad
satyam-malgaya <satyam.malgaya@gmail.com>
2025-11-25 22:17:34 +0530

    updating code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,23 +3,28 @@ import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import { IoMdSearch } from "react-icons/io";
 import logo from "/logo.png"
+import { useState } from "react";
 const Navbar = () => {
+    const [search, setsearch] = useState(false)
   return (
-    
       <nav className='w-full h-20 border-1 shadow border-black justify-between flex items-center px-5'>
         <div>
            <img className="w-23 h-[15vh]" src={logo} alt="Sorry the logo was not show" />
         </div>
-       <div className='flex flex-row gap-10'>
-       <NavLink className='navli' to="/">Home</NavLink>
-       <NavLink className='navli' to="/about">About</NavLink>
-       <NavLink className='navli' to="/contact">Contact</NavLink>
-       <NavLink className='navli' to="/loan-calculator">Loan Calculator</NavLink>
-       <NavLink className='navli' to="/careers">Careers</NavLink>
+       <div className='flex flex-row gap-10  text-[18px]'>
+       <NavLink className={styles.navlink} to="/">Home</NavLink>
+       <NavLink className={styles.navlink} to="/about">About</NavLink>
+       <NavLink className={styles.navlink} to="/contact">Contact</NavLink>
+       <NavLink className={styles.navlink} to="/loan-calculator">Loan Calculator</NavLink>
+       <NavLink className={styles.navlink} to="/careers">Careers</NavLink>
        </div>
-       <div>
-         <button>logIn</button>
-         <button><IoMdSearch /></button>
+       <div className="flex flex-row gap-4">
+         <button onClick={()=>{
+               
+         }} className="px-[11px] py-[11px] rounded-full bg-[#170C52] text-white "><IoMdSearch className="text-[18px] hover:text-[20px]" /></button>
+         <button 
+           id={styles.navbtn}
+         className=" px-5 py-2  rounded-lg text-center border-1 border-black  text-black font-medium">LogIn</button>
        </div>
       </nav>
 
