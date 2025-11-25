7c1b4c99cfe768947aaff2f271cbe08933611372
satyam-malgaya <satyam.malgaya@gmail.com>
2025-11-25 16:34:48 +0530

    adding folder

--- src/Routes/allroutes.jsx
+++ src/Routes/allroutes.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const allroutes = () => {
   return (
     <div>
-        
+      
     </div>
   )
 }
 
-export default Home
+export default allroutes
