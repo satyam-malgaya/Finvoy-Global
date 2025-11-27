325e4cbe2e72f6ecdace1f563017ccf35cb5b9f7
Hariom Choudhary <165458145+Hariom93@users.noreply.github.com>
2025-11-27 15:32:28 +0530

    delete career file

--- src/Routes/allroutes.jsx
+++ src/Routes/allroutes.jsx
@@ -4,7 +4,6 @@ import Home from '../pages/Home'
 import About from '../pages/About'
 import Contact from '../pages/Contact'
 import LoanCalculator from '../pages/LoanCalculator'
-import Careers from '../pages/Careers'
 import LoanDetails from '../pages/LoanDetails'
 import LoanPorduct from '../pages/LoanPorduct'
 const Allroutes = () => {
@@ -16,7 +15,6 @@ const Allroutes = () => {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/loan-calculator" element={<LoanCalculator />} />
-        <Route path="/careers" element={<Careers />} />
         <Route path="/loan/:loanId" element={<LoanDetails />} />
       </Routes>
     </div>
